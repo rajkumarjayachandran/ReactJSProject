809d23f8b59ae01ce49707aaba9ce6f1b2916733
unknown <rajkumar.jayachandran@gmail.com>
2025-04-24 10:53:42 +0530

    new typescript proect

---
+++ src/Components/Helloworld.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Helloworld: React.FC = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px' }}>
+            {/* First Layout */}
+            <div style={{ border: '1px solid black', padding: '20px', width: '40%' }}>
+                <h2>First Layout</h2>
+                <label htmlFor="firstInput">First Input:</label>
+                <input id="firstInput" type="text" style={{ display: 'block', marginTop: '10px' }} />
+            </div>
+
+            {/* Second Layout */}
+            <div style={{ border: '1px solid black', padding: '20px', width: '40%' }}>
+                <h2>Second Layout</h2>
+                <label htmlFor="secondInput">Second Input:</label>
+                <input id="secondInput" type="text" style={{ display: 'block', marginTop: '10px' }} />
+            </div>
+        </div>
+    );
+};
+
+interface FuncNameProps {
+    param1: string;
+    param2: number;
+}
+
+const FuncName: React.FC<FuncNameProps> = ({ param1, param2 }) => {
+    return (
+        <div>
+            Hello {param1}, you have {param2} new messages.
+        </div>
+    );
+};
+
+export default Helloworld ;
+export { FuncName};
---
+++ src/data.tsx
@@ -0,0 +1,17 @@
+export const sculptureList = [{
+    name: 'Homenaje a la Neurocirugía',
+    artist: 'Marta Colvin Andrade',
+    description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.'
+    }, {
+    name: 'Floralis Genérica',
+    artist: 'Eduardo Catalano',
+    description: 'This enormous (75 ft. or 23m) silver flower is located in Buenos Aires. It is designed to move, closing its petals in the evening or when strong winds blow and opening them in the morning.'
+  }, {
+    name: 'Eternal Presence',
+    artist: 'John Woodrow Wilson',
+    description: 'Wilson was known for his preoccupation with equality, social justice, as well as the essential and spiritual qualities of humankind. This massive (7ft. or 2,13m) bronze represents what he described as "a symbolic Black presence infused with a sense of universal humanity."'
+     }, {
+    name: 'Moai',
+    artist: 'Unknown Artist',
+    description: 'Located on the Easter Island, there are 1,000 moai, or extant monumental statues, created by the early Rapa Nui people, which some believe represented deified ancestors.'
+ }];
