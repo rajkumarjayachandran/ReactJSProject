df615414cf9588fc09acc70d2201c16fcc986c64
Rajkumar <rajkumar.jayachandran@gmail.com>
2025-04-25 12:34:08 +0530

    new

--- src/data.tsx
+++ src/data.tsx
@@ -15,7 +15,7 @@ export const sculptureList = [{
     artist: 'Unknown Artist',
     description: 'Located on the Easter Island, there are 1,000 moai, or extant monumental statues, created by the early Rapa Nui people, which some believe represented deified ancestors.'
  }, {
-  name: 'Koi koi',
+  name: 'Koi koi1',
   artist: 'New join Artist',
   description: 'Located on the Easter Island, there are 1,000 moai, or extant monumental statues, created by the early Rapa Nui people, which some believe represented deified ancestors.'
 }];
